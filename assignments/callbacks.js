0db9d41ce519e89d97ca282824a58d13f5bdedf2
Kerri-Ann <kaybee1jam@yahoo.com>
2019-09-17 16:39:28 -0400

    completed last challange for callback.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -80,7 +80,13 @@ console.log(testMultiplyNums);
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
+
+const testContains = contains('Gum', items, (total) => {
+  return total;
+});
+console.log(testContains);
 
 /* STRETCH PROBLEM */
 
